25ad2dd16ce295c44ad9e9634a30b3a93cdecda0
Felipeonline <119813692+Felipeonline@users.noreply.github.com>
2023-10-20 18:47:57 -0300

    Add files via upload

---
+++ Lab01-2023-1-main-FELIPE/Lab01-2023-1-main/src/main/App.java
@@ -0,0 +1,15 @@
+package main;
+public class App {
+    public static int encontre(int[] arrayOrdenado, int num){
+        int a = 0;
+        return -1;
+    }
+
+    public static int maior(int[] arrayDesordenado){
+        return -1;
+    }
+
+    public static Integer parMaisProximo(Integer[][] pares){
+        return 1;
+    }
+}
